2ad1efd2f451659e35fa2ec662648a0c33d98e76
FelipePaterni <felipesp.chaves@gmail.com>
2026-03-26 15:43:56 -0300

    lista

--- 26-03/lista.py
+++ 26-03/lista.py
@@ -1,3 +1,4 @@
+# Exemplo 1
 predio = [
     "terreo",
     "primeiro andar",
@@ -21,6 +22,7 @@ predio.append(input("novo andar: "))
 
 print(predio)
 
+# Exemplo 2
 notas = [6, 7, 5, 8, 9]
 soma = 0
 x = 0
@@ -28,14 +30,26 @@ for n in notas:
     soma += n
 print("Media: %.2f" % (soma / len(notas)))
 
-
+# Exemplo 3
 numeros = [0, 0, 0, 0, 0]
 x = 0
 while x < 5:
     numeros[x] = int(input("Número %d: " % (x + 1)))
     x += 1
 while True:
-    escolhido = int(input("Que posição você quer imprimir (o para sair): "))
+    escolhido = int(input("Que posição você quer imprimir (0 para sair): "))
     if escolhido == 0:
         break
     print("Você escolheu o número: %d" % (numeros[escolhido - 1]))
+
+# Exemplo 4
+v = [1,2,3,4,5,6]
+l = v
+
+print(v)
+print(l)
+
+l[0] = 9
+
+print(v)
+print(l)
